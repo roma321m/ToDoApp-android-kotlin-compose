e0129d7b95f0d073b64a9468332c8562b3e833b4
Roman Michailov <roman.michailov@intel.com>
2022-08-18 13:04:49 +0300

    Created list screen and added Fab button to it

--- app/src/main/java/com/example/todoapp_android_kotlin_compose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/example/todoapp_android_kotlin_compose/navigation/destinations/ListComposable.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.todoapp_android_kotlin_compose.ui.screens.list.ListScreen
 import com.example.todoapp_android_kotlin_compose.util.Constants.LIST_ARGUMENT_KEY
 import com.example.todoapp_android_kotlin_compose.util.Constants.LIST_SCREEN
 
@@ -17,6 +18,6 @@ fun NavGraphBuilder.listComposable (
             type = NavType.StringType
         })
     ){
-        // TODO - ui for this screen
+        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
     }
 }
